13279de37181618b8f38ae403c1a9e0eb2e339be
Bug-insect77 <Bug-insect77>
2025-06-26 15:34:17 +0530

    Practice set 4 completed

--- 4_List_Tuples/practice_Set_4/PS.py
+++ 4_List_Tuples/practice_Set_4/PS.py
@@ -28,10 +28,28 @@ my_list =[]
 # print(my_list)
 
 # 2. Write a program to accept marks of 6 students and display them in a sorted manner. 
+# my_list =[]
+# my_list.append(int(input("Enter Marks: ")))
+# my_list.append(int(input("Enter Marks: ")))
+# my_list.append(int(input("Enter Marks: ")))
+# my_list.append(int(input("Enter Marks: ")))
+# my_list.append(int(input("Enter Marks: ")))
+# my_list.append(int(input("Enter Marks: ")))
+# my_list.sort()
+# print(sorted(my_list))
+# print(my_list)
 
 # 3. Check that a tuple type cannot be changed in python. 
+# my_tuple = (1,2,3,4)
+# my_tuple[3] =5
+# TypeError: 'tuple' object does not support item assignment
+
 # 4. Write a program to sum a list with 4 numbers. 
+my_list = [4,5,6,7]
+print(sum(my_list))
+
+
 # 5. Write a program to count the number of zeros in the following tuple: 
-# a = (7, 0, 8, 0, 0, 9) 
- 
+a = (7, 0, 8, 0, 0, 9) 
+print(a.count(0))
   
